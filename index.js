12f778c40babc584154a6ad56c7ba36818e2c3f9
siahxw <siahw@proton.me>
2022-07-15 20:37:01 -0400

    Added code that plays a single round

--- index.js
+++ index.js
@@ -23,3 +23,59 @@ If number equals 0 then "Rock" will be returned; if number equals
 will be returned */
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "ROCK":
+            if (computerSelection == "ROCK") {
+                console.log("Draw! You both selected Rock.")
+                return "DRAW"
+            }
+            if (computerSelection == "PAPER") {
+                console.log("You lose! Computer selected Paper. Paper beats Rock.")
+                return "COMPUTER"
+            }
+            if (computerSelection == "SCISSORS") {
+                console.log("You won! Rock beats Scissors.")
+                return "USER"
+            }
+            break;
+    }
+
+    switch (playerSelection) {
+        case "PAPER":
+            if (computerSelection == "ROCK") {
+                console.log("You won! Paper beats Rock.")
+                return "USER"
+            }
+            if (computerSelection == "PAPER") {
+                console.log("Draw! You both selected Paper.")
+                return "DRAW"
+            }
+            if (computerSelection == "SCISSORS") {
+                console.log("You lose! Computer selected Scissors. Scissors beats Paper.")
+                return "COMPUTER"
+            }
+            break;
+    }
+
+    switch (playerSelection) {
+        case "SCISSORS":
+            if (computerSelection == "ROCK") {
+                console.log("You lose! Computer selected Rock. Rock beats Scissors.")
+                return "COMPUTER"
+            }
+            if (computerSelection == "PAPER") {
+                console.log("You won! Scissors beats Rock.")
+                return "USER"
+            }
+            if (computerSelection == "SCISSORS") {
+                console.log("Draw! You both selected Scissors.")
+                return "DRAW"
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
